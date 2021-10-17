9cd475b865e57f82dbdbbb587ca4e56a3710e460
sunilbn12@gmail.com <sunilbn12@gmail.com>
2021-10-17 12:48:58 +0530

    first commit

---
+++ target/test-classes/jsons/BatterItem.java
@@ -0,0 +1,20 @@
+public class BatterItem{
+	private String id;
+	private String type;
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setType(String type){
+		this.type = type;
+	}
+
+	public String getType(){
+		return type;
+	}
+}
